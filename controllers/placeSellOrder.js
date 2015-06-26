ff14f228806c5883b410cea5e19de5d97e46a517
Naila Nur <nnur3@uwo.ca>
2015-06-26 01:17:52 -0400

    stocks

--- controllers/placeSellOrder.js
+++ controllers/placeSellOrder.js
@@ -2,7 +2,7 @@ StockMarketApplication.PlaceSellOrderController = Ember.ObjectController.extend(
     //needs: ['marketDepth'],
     logoUrl: function() {
         //TODO: whatever this first gets set to never changes. but should change when controllers.marketdepth.model changes :(
-        var company = this.get('model'); //this.get('controllers.marketDepth.model');
+        company = this.get('model'); //this.get('controllers.marketDepth.model');
         var symbol = company.get('symbolURL');
         return symbol;
     }.property('controllers.marketDepth.model'),
@@ -16,45 +16,53 @@ StockMarketApplication.PlaceSellOrderController = Ember.ObjectController.extend(
             var price = parseFloat(this.get('price'));
             var volume = parseInt(this.get('volume'));
 
+            var buyOrders = this.store.all('buyOrder')
+            var bids = buyOrders.get('content');
+
+            console.log(company.get('price'));
+            console.log(price);
+
             this.store.createRecord('sellOrder', {
                 price: price,
                 volume: volume
             });
 
-            var bids = this.get('model').get('buyOrders');
-            console.log(bids);
-
-            bids.forEach(function(item, index) {
-                console.log(item);
-            });
 
 
 
 
-            if (volume > sharesVolume) {
 
-                item.set('numberOfShares', volume - sharesVolume);
-                var currentVolume = company.get('shareVolume');
-                company.set('shareVolume', parseInt(currentVolume) + parseInt(sharesVolume));
-                sharesVolume = 0;
+            // for (var i = 0; i < companies.length; i++) {
 
-            } else if (volume == sharesVolume) {
 
-                var currentVolume = company.get('shareVolume');
-                company.set('shareVolume', parseInt(currentVolume) + parseInt(sharesVolume));
-                sharesVolume = 0;
-                toDelete.push(item);
 
-            } else if (volume < sharesVolume) {
+            //     if (volume > sharesVolume) {
 
-                var currentVolume = company.get('shareVolume');
-                company.set('shareVolume', parseInt(currentVolume) + parseInt(volume));
-                sharesVolume = sharesVolume - volume;
-                toDelete.push(item);
+            //         item.set('numberOfShares', volume - sharesVolume);
+            //         var currentVolume = company.get('volume');
+            //         company.set('shareVolume', parseInt(currentVolume) + parseInt(sharesVolume));
+            //         sharesVolume = 0;
 
-            }
+
+            //     } else if (volume == sharesVolume) {
+
+            //         var currentVolume = company.get('shareVolume');
+            //         company.set('shareVolume', parseInt(currentVolume) + parseInt(sharesVolume));
+            //         sharesVolume = 0;
+            //         toDelete.push(item);
+
+            //     } else if (volume < sharesVolume) {
+
+            //         var currentVolume = company.get('shareVolume');
+            //         company.set('shareVolume', parseInt(currentVolume) + parseInt(volume));
+            //         sharesVolume = sharesVolume - volume;
+            //         toDelete.push(item);
+
+            //     }
 
         }
 
     }
+
+
 });
--- routes/application.js
+++ routes/application.js
@@ -1,15 +1,15 @@
-/**
- * Created by Abdelkader on 2015-02-03.
- */
-StockMarketApplication.ApplicationRoute = Ember.Route.extend({
-    model: function(params) {
-        return this.store.find('post', params.post_id);
-    }
-    renderTemplate: function() {
-        this._super();
-        this.render('main', {
-            outlet: 'main'
-        });
-    }
+// /**
+//  * Created by Abdelkader on 2015-02-03.
+//  */
+// StockMarketApplication.ApplicationRoute = Ember.Route.extend({
+//     model: function(params) {
+//         // return this.store.find('post', params.post_id);
+//     }
+//     renderTemplate: function() {
+//         this._super();
+//         this.render('main', {
+//             outlet: 'main'
+//         });
+//     }
 
-});
+// });
